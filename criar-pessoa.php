1f88e34bbab06f6a72a28205b5e200478f4d7936
sasagrant <crav0@proton.me>
2025-10-22 20:27:30 -0300

    eita....

--- criar-pessoa.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "db.php";
-$insert_stmt = mysqli_prepare($db, "insert into bd2.pessoas (nome, email) values (?, ?)");
-mysqli_stmt_bind_param($insert_stmt, 'ss', $_POST["$tabela_login-nome"], $_POST["$tabela_login-email"]);
-mysqli_stmt_execute($insert_stmt);
